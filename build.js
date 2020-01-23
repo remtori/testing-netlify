4547068d19354577559fe002a686084a59d0a924
Remtori <lqv.remtori@gmail.com>
2020-01-24 00:42:29 +0700

    Minor adjustment

--- build.js
+++ build.js
@@ -3,16 +3,15 @@ const cache = require('./netlify-cache');
 
 (async function() {
 
-	console.log('All Netlify ENVIRONMENT:');
-	console.log(JSON.stringify(process.env));
 	await cache.preBuild();
 
 	await fs.ensureDir('./build');
 	const files = await fs.readdir('./build');
+	console.log(`Remtori: Found ${files.length} files in build directory`);
 
 	await fs.writeFile(
 		`./build/index-${files.length}.html`,
-		`<div>This is the ${files.length}-th generated file</div>`
+		`<div>This is the ${files.length}-th generated file</div>`,
 	);
 
 	await cache.postBuild();
