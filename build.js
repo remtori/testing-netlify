5a9533fce812ee3d8583ac11f5b9cd7ab605aa6a
Remtori <lqv.remtori@gmail.com>
2020-01-24 15:54:04 +0700

    Fetching remote data

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const fetch = require('node-fetch');
 const cache = require('./netlify-cache');
 
 (async function() {
@@ -9,9 +10,17 @@ const cache = require('./netlify-cache');
 	const files = await fs.readdir('./build');
 	console.log(`Remtori: Found ${files.length} files in build directory`);
 
+	const remoteData = await fetch(
+		`https://firestore.googleapis.com/v1/projects/remtori/databases/(default)/documents/blogs`
+	).then(r => r.text());
+
 	await fs.writeFile(
 		`./build/index-${files.length}.html`,
-		`<div>This is the ${files.length}-th generated file</div>`,
+		`
+			<div>This is the ${files.length}-th generated file</div>
+			<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script>
+			<pre><code class="prettyprint">${remoteData}</code></pre>
+		`,
 	);
 
 	await cache.postBuild();
