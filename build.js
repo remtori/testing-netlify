67bf2f68e24b749d7e22123f87fca110426e8157
Netlify CI <netlify@netlify.com>
2020-02-19 00:52:57 +0700

    Simlify build script

--- build.js
+++ build.js
@@ -11,16 +11,10 @@ const cache = require('./netlify-cache');
 		const files = await fs.readdir('./build');
 		console.log(`Remtori: Found ${files.length} files in build directory`);
 
-		const remoteData = await fetch(
-			`https://firestore.googleapis.com/v1/projects/remtori/databases/(default)/documents/blogs`
-		).then(r => r.text());
-
 		await fs.writeFile(
 			`./build/index-${files.length}.html`,
 			`
 				<div>This is the ${files.length}-th generated file</div>
-				<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script>
-				<pre><code class="prettyprint">${remoteData}</code></pre>
 			`,
 		);
 
